d1981775b07a7f45c446a8578820a9d93bef1b7d
Yona Silveira <yona.nantes22@gmail.com>
2023-05-07 20:31:26 -0300

    add about text and image

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+import ProfilePic from '../../images/profile-pic.png'
 
 const About = props => {
     return (
         <section>
-            <Container>
+            <Container fluid>
                 <h3>Sobre</h3>
                 <Row>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
+                    <Col>
+                        <Image fluid src={ProfilePic}/>
+                    </Col>
+                    <Col className="text-start justify-content-center align-items-center d-flex">
+                        <p>Profissional de Front-end 4 anos de experiência em JavaScript, HTML e CSS e com 1 ano de experiência em React e React Native. Foco em aprendizado constante e atualização de ferramentas. Possui habilidades em Python, HTML5 semântico, CSS3, Styled-Components, Design Responsivo, plataformas LMS, Node, Gulp, Git e metodologias ágeis, boas práticas, JavaScript(ES6), React.js, React-Native e mobile first. Busca constante de melhoria como profissional mantendo-se atualizada. Sou uma pessoa muito proativa e gosto muito de compartilhar conhecimentos e ser desafiada.</p>
+                    </Col>
                 </Row>
             </Container>
         </section>
